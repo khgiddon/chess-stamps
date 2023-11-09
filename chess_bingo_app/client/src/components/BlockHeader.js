8d8b158380bcfe24654a51f6097f588bdd725381
kgiddon@jobcase.com <kgiddon@jobcase.com>
2023-11-08 22:08:56 -0500

    logo/header change

--- chess_bingo_app/client/src/components/BlockHeader.js
+++ chess_bingo_app/client/src/components/BlockHeader.js
@@ -3,15 +3,10 @@ import React from 'react';
 const BlockHeader = () => (
   <div>
     <div className="stampImageContainer">
-    <img src="assets/stamp_images/image.webp" alt="Stamp" className='stampImage'/>
-      <img src="assets/stamp_images/image (1).webp" alt="Stamp" className='stampImage'/>
-      <img src="assets/stamp_images/image (5).webp" alt="Stamp" className='stampImage'/>          
-      <img src="assets/stamp_images/image (6).webp" alt="Stamp" className='stampImage'/>
-      <img src="assets/stamp_images/image (4).webp" alt="Stamp" className='stampImage'/>
+    <img src="assets/stamp_images/logo3.png" alt="Chess Opening Stamp Collector" className='stampImage'/>
     </div>
-    <h1>Chess Opening Stamp Collector</h1>
     {console.log('Header rendered')}
-    <h3>How many chess opening stamps have you collected?</h3>
+    <h3>How many chess opening stamps have <u>you</u> collected?</h3>
   </div>
 );
 
