ef08f0ff3681d434ecce7a214eb271957435b591
kgiddon@jobcase.com <kgiddon@jobcase.com>
2023-11-16 23:41:48 -0500

    use CSS

--- chess_bingo_app/client/src/components/Row.js
+++ chess_bingo_app/client/src/components/Row.js
@@ -2,69 +2,27 @@ import React from 'react';
 import ChessImage from './ChessImage';
 
 const Row = ({ label, label_description, opening, fen, id, text, icon }) => {
-
-
-  const labelRowStyle = {
-    display: 'grid',
-    alignItems: 'center', // Center items vertically
-    justifyContent: 'center', // Center items horizontally
-    backgroundColor: '#3A75C4 ', // Background color for the label row
-    color: 'white', // Text color for the label row
-    fontWeight: 'bold', // Bold text for the label row
-    padding: '0px 0', // Vertical padding for the label row
-    textTransform: 'uppercase', // Transform label to uppercase
-    fontSize: '1em', // Adjust font size as needed
-  };
-
-  const descriptionStyle = {
-    textTransform: 'lowercase', // Transform label description to lowercase
-    fontSize: '0.9em', // Maintain font size for consistency
-    fontWeight: 'bold', // Bold text for consistency
-  };
-
-  const chessImageStyle = {
-    gridArea: '1 / 1 / 2 / 2',
-    maxWidth: '225px',
-    maxHeight: '225px',
-    padding: '10px',
-    boxSizing: 'border-box',
-  };
-
-  const textStyle = {
-    gridArea: '1 / 2 / 2 / 3',
-    maxWidth: '70%',
-    padding: '10px',
-  };
-
-  const linkStyle = {
-    textDecoration: 'none',
-    color: '#000',
-  };
-
   return (
-    <div>
-      {/* Small row for label and description */}
-      <div style={labelRowStyle}>
+    <div className="row">
+      <div className="label-row">
         <span>
           {label}
         </span>
         {label_description && (
-                  <div>
-                    <span style={descriptionStyle}>&nbsp;{`${label_description}`}</span>
-                  </div>
+          <div>
+            <span className="description">&nbsp;{`${label_description}`}</span>
+          </div>
         )}
-
       </div>
-      {/* Existing row content */}
       <div style={{ display: 'grid', gridTemplateColumns: '1fr 2fr', alignItems: 'flex-start' }}>
-        <div style={chessImageStyle}>
-          <a href={`https://lichess.org/analysis/${fen}`} style={linkStyle}>
+        <div className="chess-image">
+          <a href={`https://lichess.org/analysis/${fen}`} className="link">
             <ChessImage fen={fen} id={id} />
           </a>
         </div>
-        <div style={textStyle}>
-          <a href={`https://lichess.org/analysis/${fen}`} style={linkStyle}>
-          <span className="openingHighlight">{opening}</span>
+        <div className="text">
+          <a href={`https://lichess.org/analysis/${fen}`} className="link">
+            <span className="openingHighlight">{opening}</span>
           </a>
           <div>{text}</div>
         </div>
