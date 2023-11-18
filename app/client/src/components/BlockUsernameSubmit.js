551bb9e93dc95c0167d3ad0d8a254209d9ba9f38
kgiddon@jobcase.com <kgiddon@jobcase.com>
2023-11-18 15:20:08 -0500

    styling and requirements

--- app/client/src/components/BlockUsernameSubmit.js
+++ app/client/src/components/BlockUsernameSubmit.js
@@ -1,6 +1,38 @@
 import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
-const BlockUsernameSubmit = ({ username, setUsername, handleSubmit }) => {
+const Container = styled.div`
+  margin: 1em;
+  padding: 1em;
+  border: 1px solid #ccc;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const StyledInput = styled.input`
+  flex: 1;
+  margin-right: 1em;
+  padding: 0.5em;
+  border-radius: 4px;
+  border: 1px solid #ccc;
+`;
+
+const StyledButton = styled.button`
+  padding: 0.5em 1em;
+  background-color: #007bff;
+  color: white;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #0056b3;
+  }
+`;
+
+const BlockUsernameSubmit = ({ username = '', setUsername, handleSubmit }) => {
   const [inputValue, setInputValue] = useState(username);
 
   const handleButtonClick = () => {
@@ -8,17 +40,28 @@ const BlockUsernameSubmit = ({ username, setUsername, handleSubmit }) => {
     handleSubmit(inputValue);
   };
 
+  const handleChange = ({ target: { value } }) => {
+    setInputValue(value);
+  };
+
   return (
-    <div className="usernameContainer">
-      <input
+    <Container>
+      <StyledInput
         type="text"
         placeholder="Enter your lichess username"
         value={inputValue}
-        onChange={(e) => setInputValue(e.target.value)}
+        onChange={handleChange}
+        required
       />
-      <button onClick={handleButtonClick}>Submit</button>
-    </div>
+      <StyledButton onClick={handleButtonClick}>Analyze</StyledButton>
+    </Container>
   );
+};
+
+BlockUsernameSubmit.propTypes = {
+  username: PropTypes.string,
+  setUsername: PropTypes.func.isRequired,
+  handleSubmit: PropTypes.func.isRequired,
 };
 
 export default BlockUsernameSubmit;
