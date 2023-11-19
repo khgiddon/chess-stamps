e079daca8c3af8cab3ef7c69e17b718dd9a5d553
kgiddon@jobcase.com <kgiddon@jobcase.com>
2023-11-19 15:58:09 -0500

    convert links to pgn instead of fen

--- app/client/src/components/DisplayTable.js
+++ app/client/src/components/DisplayTable.js
@@ -28,6 +28,7 @@ const DisplayTable = ({ data }) => {
         label="Rarest stamp"
         opening={data.most_obscure_stamp.name}
         fen={data.most_obscure_stamp.fen}
+        pgn={data.most_obscure_stamp.pgn}
         id={2}
         text={`You've played this ${data.most_obscure_stamp.player_total_with_children} times. This is only ${percentageToOneInEveryX(data.most_obscure_stamp.all_pct)} Lichess stamps! What a find!`}
       />
@@ -36,6 +37,7 @@ const DisplayTable = ({ data }) => {
         label_description="(most played relative to population)"
         opening={data.most_popular_white.name}
         fen={data.most_popular_white.fen}
+        pgn={data.most_popular_white.pgn}
         id={3}
         text={`You've played this ${data.most_popular_white.player_white_with_children} times, or ${percentageToOneInEveryX(data.most_popular_white.player_pct_with_children)} stamps. This is only ${percentageToOneInEveryX(data.most_popular_white.all_pct)} of all Lichess stamps. You play it ${customRoundForRatio(data.most_popular_white.player_pct_with_children/data.most_popular_white.all_pct)}x as frequently as frequently as the population.`}
       />
@@ -44,55 +46,60 @@ const DisplayTable = ({ data }) => {
         label_description="(most played relative to population)"
         opening={data.most_popular_black.name}
         fen={data.most_popular_black.fen}
+        pgn={data.most_popular_black.pgn}
         id={4}
         text={`You've played this ${data.most_popular_black.player_black_with_children} times, or ${percentageToOneInEveryX(data.most_popular_black.player_pct_with_children)} stamps. This is only ${percentageToOneInEveryX(data.most_popular_black.all_pct)} of all Lichess stamps. You play it ${customRoundForRatio(data.most_popular_black.player_pct_with_children/data.most_popular_black.all_pct)}x as frequently as frequently as the population.`}
       />
-<MemoizedRow
-  label="Repertoire: white"
-  label_description="(most played relative to population, min. 10 games)"
-  opening={data.most_popular_white_min10.name}
-  fen={data.most_popular_white_min10.fen}
-  id={5}
-  text={data.most_popular_white_min10.name !== 'None' ? 
-        `You've played this ${data.most_popular_white_min10.player_white_with_children} times,
-         or ${percentageToOneInEveryX(data.most_popular_white_min10.player_pct_with_children)} stamps.
-        That means you play it ${customRoundForRatio(data.most_popular_white_min10.player_pct_with_children/data.most_popular_white_min10.all_pct)}x as frequently as the population.`
-        : "You haven't played any opening as white ten times."}
-/>
-<MemoizedRow
-  label="Repertoire: black"
-  label_description="(most played relative to population, min. 10 games)"
-  opening={data.most_popular_black_min10.name}
-  fen={data.most_popular_black_min10.fen}
-  id={6}
-  text={data.most_popular_black_min10.name !== 'None' ?
-        `You've played this ${data.most_popular_black_min10.player_black_with_children} times,
-        or ${percentageToOneInEveryX(data.most_popular_black_min10.player_pct_with_children)} stamps.
-        That means you play it ${customRoundForRatio(data.most_popular_black_min10.player_pct_with_children/data.most_popular_black_min10.all_pct)}x as frequently as the population.`
-        : "You haven't played any opening as black ten times."}
-/>
-<MemoizedRow
-  label="Random collected stamp"
-  opening={data.random_collected.name}
-  fen={data.random_collected.fen}
-  id={7}
-  text={data.random_collected.name !== 'None' ?
-        `You've played this ${data.random_collected.player_total_with_children} times,
-        or ${percentageToOneInEveryX(data.random_collected.player_pct_with_children)} stamps.
-        This is played ${percentageToOneInEveryX(data.random_collected.all_pct)} Lichess stamps.
-        You play it ${customRoundForRatio(data.random_collected.player_pct_with_children/data.random_collected.all_pct)}x as frequently as frequently as the population.
-        `
-        : "Error."}
-/>
-<MemoizedRow
-  label="Random missing stamp"
-  opening={data.random_missing.name}
-  fen={data.random_missing.fen}
-  id={8}
-  text={data.random_missing.name !== 'None' ?
-        `This is played ${percentageToOneInEveryX(data.random_missing.all_pct)} Lichess stamps, but you've played this not once.`
-        : "Error."}
-/>
+    <MemoizedRow
+      label="Repertoire: white"
+      label_description="(most played relative to population, min. 10 games)"
+      opening={data.most_popular_white_min10.name}
+      fen={data.most_popular_white_min10.fen}
+      pgn={data.most_popular_white_min10.pgn}
+      id={5}
+      text={data.most_popular_white_min10.name !== 'None' ? 
+            `You've played this ${data.most_popular_white_min10.player_white_with_children} times,
+            or ${percentageToOneInEveryX(data.most_popular_white_min10.player_pct_with_children)} stamps.
+            That means you play it ${customRoundForRatio(data.most_popular_white_min10.player_pct_with_children/data.most_popular_white_min10.all_pct)}x as frequently as the population.`
+            : "You haven't played any opening as white ten times."}
+    />
+    <MemoizedRow
+      label="Repertoire: black"
+      label_description="(most played relative to population, min. 10 games)"
+      opening={data.most_popular_black_min10.name}
+      fen={data.most_popular_black_min10.fen}
+      pgn={data.most_popular_black_min10.pgn}
+      id={6}
+      text={data.most_popular_black_min10.name !== 'None' ?
+            `You've played this ${data.most_popular_black_min10.player_black_with_children} times,
+            or ${percentageToOneInEveryX(data.most_popular_black_min10.player_pct_with_children)} stamps.
+            That means you play it ${customRoundForRatio(data.most_popular_black_min10.player_pct_with_children/data.most_popular_black_min10.all_pct)}x as frequently as the population.`
+            : "You haven't played any opening as black ten times."}
+    />
+    <MemoizedRow
+      label="Random collected stamp"
+      opening={data.random_collected.name}
+      fen={data.random_collected.fen}
+      pgn={data.random_collected.pgn}
+      id={7}
+      text={data.random_collected.name !== 'None' ?
+            `You've played this ${data.random_collected.player_total_with_children} times,
+            or ${percentageToOneInEveryX(data.random_collected.player_pct_with_children)} stamps.
+            This is played ${percentageToOneInEveryX(data.random_collected.all_pct)} Lichess stamps.
+            You play it ${customRoundForRatio(data.random_collected.player_pct_with_children/data.random_collected.all_pct)}x as frequently as frequently as the population.
+            `
+            : "Error."}
+    />
+    <MemoizedRow
+      label="Random missing stamp"
+      opening={data.random_missing.name}
+      fen={data.random_missing.fen}
+      pgn={data.random_missing.pgn}
+      id={8}
+      text={data.random_missing.name !== 'None' ?
+            `This is played ${percentageToOneInEveryX(data.random_missing.all_pct)} Lichess stamps, but you've never played this.`
+            : "Error."}
+    />
 
     </div>
   );
--- app/client/src/components/Row.js
+++ app/client/src/components/Row.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ChessImage from './ChessImage';
 
-const Row = ({ label, label_description, opening, fen, id, text, icon }) => {
+const Row = ({ label, label_description, opening, fen, id, text, pgn }) => {
   return (
     <div className="row">
       <div className="label-row">
@@ -13,12 +13,12 @@ const Row = ({ label, label_description, opening, fen, id, text, icon }) => {
         )}
       </div>
         <div className="chess-image">
-          <a href={`https://lichess.org/analysis/${fen}`} className="link">
+          <a href={`https://lichess.org/analysis/pgn/${[pgn]}`} className="link">
             <ChessImage fen={fen} id={id} />
           </a>
         </div>
         <div className="text">
-          <a href={`https://lichess.org/analysis/${fen}`} className="link">
+          <a href={`https://lichess.org/analysis/pgn/${[pgn]}`} className="link">
             <span className="openingHighlight">{opening}</span>
           </a>
           <div>{text}</div>
