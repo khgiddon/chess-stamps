2d4d60ada23ad19a0280f620c69dd86ed98f695c
kgiddon@jobcase.com <kgiddon@jobcase.com>
2023-11-21 18:00:17 -0500

    fix bug where progress bar could exceed 100

--- app/client/src/components/ProgressBar.js
+++ app/client/src/components/ProgressBar.js
@@ -6,13 +6,13 @@ const ProgressWrapper = ({ progress, gamesexpected, username }) => (
     <span>
         Loading data and analyzing stamps for&nbsp;<b>{gamesexpected}</b>&nbsp;games played by&nbsp;<b>{username}</b>
     </span>
-    {progress <= 100 && (
+    { (
             <img src="assets/misc_images/pawn.png" alt="Loading..." className="chess-piece" />
     )}
     </div>
     <div className="progress-bar-container">
       <div className="progress-bar" style={{ width: `${progress}%` }}></div>
-      <div className="progress-text">{progress}%</div>
+      <div className="progress-text">{progress > 100 ? 100 : progress}%</div>
     </div>
   </div>
 );
