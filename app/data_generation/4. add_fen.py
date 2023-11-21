15ae6bb0fbc840b4c1b825f709330b0d8e5db767
kgiddon@jobcase.com <kgiddon@jobcase.com>
2023-11-20 22:43:08 -0500

    add ply to dataset

--- app/data_generation/4. add_fen.py
+++ app/data_generation/4. add_fen.py
@@ -5,7 +5,7 @@ import pandas as pd
 import requests
 from time import sleep
 
-def pgn_to_fen(pgn_text):
+def pgn_to_fen_and_ply(pgn_text):
     pgn = io.StringIO(pgn_text)
     game = chess.pgn.read_game(pgn)
     
@@ -13,13 +13,16 @@ def pgn_to_fen(pgn_text):
     board = game.board()
 
     # Traverse through all moves and play them on a board
+    ply_count = 0
     for move in game.mainline_moves():
         board.push(move)
+        ply_count += 1
 
-    return board.fen()
+    return board.fen(), ply_count
 
 openings_db = "assets/openings_pgn/combined_with_stats_parents.tsv"
 df = pd.read_csv(openings_db, sep="\t")
-df['fen'] = df['pgn'].apply(pgn_to_fen)
+
+df['fen'], df['ply'] = zip(*df['pgn'].apply(pgn_to_fen_and_ply))
 
 df.to_csv("assets/openings_pgn/combined_with_stats_parents.tsv", sep="\t", index=False)
