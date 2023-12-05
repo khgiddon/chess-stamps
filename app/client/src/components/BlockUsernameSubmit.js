14dd1eb1757544f080f7fe1b43e16fb07d322915
kgiddon@jobcase.com <kgiddon@jobcase.com>
2023-12-04 19:48:14 -0500

    timeframe UI element, not yet connected

--- app/client/src/components/BlockUsernameSubmit.js
+++ app/client/src/components/BlockUsernameSubmit.js
@@ -5,6 +5,9 @@ import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
 import Tooltip from '@mui/material/Tooltip';
 import HelpIcon from '@mui/icons-material/Help';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+
 
 const BlockUsernameSubmit = ({ username = '', setUsername, handleSubmit }) => {
   const [inputValue, setInputValue] = useState(username);
@@ -64,21 +67,49 @@ const BlockUsernameSubmit = ({ username = '', setUsername, handleSubmit }) => {
         </Box>
       </Box>
       <Box
+            display="flex"
+            flexDirection="row"
+            justifyContent="center"
+            alignItems="center"
+            marginBottom="0em"
+            marginTop="0em"
+            width="100%">
+
+      </Box>
+      <p className="smallText">
+          Check games from: 
+          <Select
+              defaultValue="last 3 months"
+              variant="standard"
+              size="small"
+              style={{ fontSize: '1em', marginLeft: '10px' }}
+            >
+              <MenuItem value="last 24 hours">last 24 hours</MenuItem>
+              <MenuItem value="last week">last week</MenuItem>
+              <MenuItem value="last month">last month</MenuItem>
+              <MenuItem value="last 3 months">last 3 months</MenuItem>
+              <MenuItem value="last 12 months">last 12 months</MenuItem>
+              <MenuItem value="forever">Forever</MenuItem>
+            </Select>
+        </p>
+      <Box
         display="flex"
         flexDirection="row"
         justifyContent="center"
         alignItems="center"
         marginBottom="0em"
         marginTop="0em"
+        paddingTop='0em'
         width="100%"
-      ><p className="smallText">
-      Or see results from <a>Magnus Carlsen</a>, <a>Hikaru Nakamura</a>, or <a>Daniel Naroditsky</a>.
-    </p>
+      >   
+        <p className="smallText">
+          Or see all-time results from <a>Magnus Carlsen</a>, <a>Hikaru Nakamura</a>, or <a>Daniel Naroditsky</a>.
+        </p>
 
 
             
 
-    </Box>
+      </Box>
      
     </Box>
   );
