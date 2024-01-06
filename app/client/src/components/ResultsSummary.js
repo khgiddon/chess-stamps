53ad0ea1e2d48d505a2239f66f8c3853d8aaa34f
kgiddon@jobcase.com <kgiddon@jobcase.com>
2024-01-06 00:32:14 -0500

    bar chart progress

--- app/client/src/components/ResultsSummary.js
+++ app/client/src/components/ResultsSummary.js
@@ -4,32 +4,61 @@ import { addThousandsSeparator } from '../utilityFunctions';
 import { BarChart } from '@mui/x-charts/BarChart';
 
 
+
+
 const ResultsSummary = ({ data, username }) => {
+
+  const chartSetting = {
+    xAxis: [
+      {
+        min: 0
+        , max: data.unique_stamps_all
+        , label: 'Played unique stamps' 
+        , tickInterval: [1000,2000,data.unique_stamps_all]
+  
+      },
+    ],
+    height: 150,
+    
+  };
+  const dataset = [
+    {
+      val: data.unique_stamps,
+      cat: username,
+    },
+    {
+      val: data.unique_stamps,
+      cat: 'Magnus Carlsen',
+    },    
+  ];
+  
+  const horizontalBars = () => {
+    return (
+      <BarChart
+        dataset={dataset}
+        yAxis={[{ scaleType: 'band', dataKey: 'cat' }]}
+        series={[{ dataKey: 'val', label: 'Played unique stamps'}]}
+        layout="horizontal"
+        slotProps={{ legend: { hidden: 'True' } }}
+        {...chartSetting}
+      />
+    );
+  }
+
   if (!data.total_stamps) return null;
 
   return (
     <div>
-      <p className="summaryText">
+      <div className="summaryText">
         Analyzed <b>{addThousandsSeparator(data.total_stamps)}</b> opening stamps from <b>{addThousandsSeparator(data.total_games)}</b> games played by <b>{username}</b>.
         <br/><br/>
         You've collected <b>{addThousandsSeparator(data.unique_stamps)}</b> unique stamps out of <b>{addThousandsSeparator(data.unique_stamps_all)}</b> possible stamps, or <b>{formatPercentage(data.unique_stamps/data.unique_stamps_all)}%</b> of all stamps.
-      </p>
+      </div>
 
-      <BarChart
-  series={[
-    { data: [data.unique_stamps], stack: 'A', label: 'Played unique stamps' },
-  ]}
-  width={400}
-  height={150}
-  xAxis={[{ min: 0
-            , max: data.unique_stamps_all
-            , label: 'Played unique stamps' 
-            , tickInterval: [1000,2000,data.unique_stamps_all]
-          }]}
-  layout="horizontal"
+      <div className="summaryChart">
+        {horizontalBars()}
+      </div>  
 
-  slotProps={{ legend: { hidden: 'True' } }}
-/>
     </div>
   );
 };
