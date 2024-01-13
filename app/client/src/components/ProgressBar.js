4acfa4b625a7ebdeabf8c9d5d59791285bdc074d
kgiddon@jobcase.com <kgiddon@jobcase.com>
2024-01-13 13:05:56 -0500

    update progress bar chess piece formatting

--- app/client/src/components/ProgressBar.js
+++ app/client/src/components/ProgressBar.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Button from '@mui/material/Button';
 import {addThousandsSeparator} from '../utilityFunctions';
+import Stack from '@mui/material/Stack';
+import Box from '@mui/material/Box';
 
 const ProgressWrapper = ({ progress, gamesexpected, username, abortController }) => {
   const cancelRequest = () => {
@@ -13,12 +15,16 @@ const ProgressWrapper = ({ progress, gamesexpected, username, abortController })
   return (
     <div className="progress-wrapper">
       <div className="progress-additional-text">
-        <span>
-          Loading data and analyzing stamps for&nbsp;<b>{addThousandsSeparator(gamesexpected)}</b>&nbsp;estimated games played by&nbsp;<b>{username}</b>
-        </span>
-        { (
-          <img src="assets/misc_images/pawn_svg.svg" alt="Loading..." className="chess-piece" />
-        )}
+        <Stack direction="column" spacing={1.5} alignItems="center">
+          <Box>
+            <img src="assets/misc_images/pawn_svg.svg" alt="Loading..." className="chess-piece" />
+          </Box>
+          <Box>
+            <span>
+              Loading data and analyzing stamps for&nbsp;<b>{addThousandsSeparator(gamesexpected)}</b>&nbsp;estimated games played by&nbsp;<b>{username}</b>
+            </span>
+          </Box>
+        </Stack>
       </div>
       <div className="progress-bar-container">
         <div className="progress-bar" style={{ width: `${progress}%` }}></div>
@@ -31,7 +37,7 @@ const ProgressWrapper = ({ progress, gamesexpected, username, abortController })
       <div className="progress-additional-text">
         <span className='progress-bar-subtitle'>
           The Lichess API is rate-limited. Depending on the number of games, this may take a few minutes.
-          <br /><br />Because the API response is streamed, game count is estimated until all data is received.
+          Game count is estimated until all data is received.
         </span>
       </div>
     </div>
