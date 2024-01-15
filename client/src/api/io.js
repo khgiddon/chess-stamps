28d48749e61d05562613b955b36c84fa74fd52bd
kgiddon@jobcase.com <kgiddon@jobcase.com>
2024-01-14 19:36:06 -0500

    make socket changes for prod env

--- client/src/api/io.js
+++ client/src/api/io.js
@@ -1,4 +1,4 @@
 import io from 'socket.io-client';
 
-
-export const socket = io('http://localhost:5000');
+const SOCKET_SERVER_URL = process.env.REACT_APP_SOCKET_SERVER_URL || 'http://localhost:5000';
+export const socket = io(SOCKET_SERVER_URL);
