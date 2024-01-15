41fed6fd96d47f7237f2e9446348f518c98ee4c1
kgiddon@jobcase.com <kgiddon@jobcase.com>
2024-01-14 20:54:49 -0500

    modify model to create table

--- models.py
+++ models.py
@@ -7,6 +7,8 @@ db = SQLAlchemy()
 
 def init_db(app):
     db.init_app(app)
+    with app.app_context():
+        db.create_all()
 
 class Record(db.Model):
     id = db.Column(db.Integer, primary_key=True)
