17a4a40ac3d9fbdc15a33d771826ff8f98fb2006
kgiddon@jobcase.com <kgiddon@jobcase.com>
2024-01-29 15:02:31 -0500

    update faq about transpositions

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -31,7 +31,7 @@ const BlockHeader = () => {
     },
     {
       question: 'Q: How do you handle transpositions?',
-      answer: 'A: When loading player data, a player will get "credit" for any opening they reach, even if they reached it via a transposition. However, the transposition looks for the canonical PGN (series of moves) for the opening, so if a player transposes through an opening but reaches that opening from a non-ordinary series of moves, the player will not receive credit. For statistics on overall popularity of openings, transpositions are ignored.',
+      answer: 'A: Each game is assigned a single opening, which is the most granular opening played, and this can be reached via transposition and assigned successfully. However, you will only get credit for a "parent" of a particular opening if you played the canonical move order for that parent opening. For example, the sequence 1. Nf3 e5 2. e4 will receive credit as the King\'s Knight Opening even though it started with Nf3, but would not receive credit for the King\'s Pawn Game because it did not play the accepted move order for that opening.',
     },
     {
       question: 'Q: When is the overall popularity data from?',
